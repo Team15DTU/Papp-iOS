c708b146432748154778a54efdab726e3419856b
niko5297 <s185082@student.dtu.dk>
2020-02-25 11:56:47 +0100

    Initial Commit

---
+++ Papp-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Papp-iOS
+//
+//  Created by Nikolaj Wassmann on 25/02/2020.
+//  Copyright © 2020 Nikolaj Wassmann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
