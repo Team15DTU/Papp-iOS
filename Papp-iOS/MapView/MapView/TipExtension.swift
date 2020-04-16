abfad6f2d37d7ba83b0c11fa58c1d305fbe6af31
niko5297 <s185082@student.dtu.dk>
2020-04-16 17:31:39 +0200

    Added toptext and animation. Removed not in use storyboards

--- Papp-iOS/MapView/MapView/TipExtension.swift
+++ Papp-iOS/MapView/MapView/TipExtension.swift
@@ -27,11 +27,12 @@ extension MapViewController
     //MARK: Support methods
     
     private func showLayout() {
+        animateLocationIcon()
         addButtons()
+        addTopText()
     }
     
     private func hideLayout() {
-        
     }
     
     private func enableMapClick() {
@@ -67,21 +68,44 @@ extension MapViewController
         confirmButton.widthAnchor.constraint(equalToConstant: 110).isActive = true
     
         
-            let cancelButton = UIButton()
-            cancelButton.setTitle("Annuller", for: .normal)
-            cancelButton.titleLabel?.font = UIFont(name: "Montserrat-Regular", size: 14)
-            cancelButton.backgroundColor = UIColor.gray
-            cancelButton.layer.cornerRadius = 20
-            view.addSubview(cancelButton)
-            
-            cancelButton.translatesAutoresizingMaskIntoConstraints = false
-            cancelButton.bottomAnchor.constraint(equalTo: mapView.layoutMarginsGuide.bottomAnchor, constant: -25).isActive = true
-            cancelButton.leftAnchor.constraint(equalTo: mapView.layoutMarginsGuide.leftAnchor, constant: 10).isActive = true
-            cancelButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
-            cancelButton.widthAnchor.constraint(equalToConstant: 110).isActive = true
+        let cancelButton = UIButton()
+        cancelButton.setTitle("Annuller", for: .normal)
+        cancelButton.titleLabel?.font = UIFont(name: "Montserrat-Regular", size: 14)
+        cancelButton.backgroundColor = UIColor.gray
+        cancelButton.layer.cornerRadius = 20
+        view.addSubview(cancelButton)
         
+        cancelButton.translatesAutoresizingMaskIntoConstraints = false
+        cancelButton.bottomAnchor.constraint(equalTo: mapView.layoutMarginsGuide.bottomAnchor, constant: -25).isActive = true
+        cancelButton.leftAnchor.constraint(equalTo: mapView.layoutMarginsGuide.leftAnchor, constant: 10).isActive = true
+        cancelButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        cancelButton.widthAnchor.constraint(equalToConstant: 110).isActive = true
+    }
+    
+    private func addTopText() {
+        let topText = UILabel()
+        topText.text = "Markér det sted hvor du vil sætte et tip"
+        topText.textAlignment = .center
+        topText.textColor = UIColor.white
+        topText.font = UIFont(name: "Montserrat-Regular", size: 14)
+        topText.backgroundColor = UIColor.init(red: 103/255, green: 150/255, blue: 190/255, alpha: 1)
+        topText.layer.cornerRadius = 20
+        topText.layer.masksToBounds = true
+        view.addSubview(topText)
         
+        topText.translatesAutoresizingMaskIntoConstraints = false
+        topText.topAnchor.constraint(equalTo: mapView.layoutMarginsGuide.topAnchor).isActive = true
+        topText.leftAnchor.constraint(equalTo: mapView.layoutMarginsGuide.leftAnchor, constant: 10).isActive = true
+        topText.rightAnchor.constraint(equalTo: mapView.layoutMarginsGuide.rightAnchor, constant: -10).isActive = true
         
+        topText.heightAnchor.constraint(equalToConstant: 50).isActive = true
+    }
+    
+    private func animateLocationIcon() {
+        [UIView.animate(withDuration: 0.3, animations: {
+            self.trackButton.transform = self.trackButton.transform.translatedBy(x: 0, y: -50)
+            self.view.layoutIfNeeded()
+        }, completion: nil)]
     }
     
     
