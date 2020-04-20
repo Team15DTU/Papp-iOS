f273add54f57c5396bfd6db8b32cdbed3d4bc9b0
Anders Lønborg <s185100@student.dtu.dk>
2020-04-20 16:17:21 +0200

    Added some test layout to TipDef.storyboard.

--- Papp-iOS/MapView/Tip/TipViewController.swift
+++ Papp-iOS/MapView/Tip/TipViewController.swift
@@ -10,12 +10,20 @@ import UIKit
 
 class TipViewController: UIViewController {
 
+    @IBOutlet weak var SendTipTextField: UITextField!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        SendTipTextField.borderStyle = UITextField.BorderStyle.roundedRect
+        SendTipTextField.layer.masksToBounds = true
+        SendTipTextField.layer.borderColor = UIColor.white.cgColor
+        SendTipTextField.layer.borderWidth = 1.0
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
 
     /*
     // MARK: - Navigation
