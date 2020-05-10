0f7eec14ab22d2058504574e50e3d575d79fabb8
Anders Lønborg <s185100@student.dtu.dk>
2020-05-11 00:01:35 +0200

    added test to TableViewControllerTests.swift

--- Papp-iOSTests/TableViewControllerTests.swift
+++ Papp-iOSTests/TableViewControllerTests.swift
@@ -7,27 +7,39 @@
 //
 
 import XCTest
+@testable import Papp_iOS
 
 class TableViewControllerTests: XCTestCase {
 
+    var sut: TableViewController!
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        let storyboard = UIStoryboard(name: "MapView", bundle: nil)
+        sut = storyboard.instantiateViewController(
+            withIdentifier: "tableViewController") as? TableViewController
+        
+        sut.loadViewIfNeeded()
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut = nil
     }
 
-    func testExample() throws {
+    func testTableViewCells() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+        
+        let tableView = sut.tableView
+        
+        XCTAssertNotNil(tableView, "no table view found")
+        
+        for i in 0...4{
+            let cell = sut.tableView(tableView!, cellForRowAt: IndexPath(row: i, section: 0))
+            XCTAssertNotNil(cell, "No cell at row \(i) (number of rows start at 0)")
+            XCTAssertNotNil(cell.imageView, "No image view in table cell on row \(i) (number of rows start at 0)")
+            XCTAssertNotNil(cell.textLabel, "No textn label in table cell on row \(i) (number of rows start at 0)")
         }
     }
-
 }
